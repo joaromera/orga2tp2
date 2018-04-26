cf6b99ea0dd5ad4be02afc0dd968670b9502479a
joa_joa <jromera@dc.uba.ar>
2018-04-26 18:35:08 -0300

    nada

--- codigo/filtros/blit_asm.asm
+++ codigo/filtros/blit_asm.asm
@@ -2,6 +2,22 @@ section .data
 DEFAULT REL
 section .text
 global blit_asm
-blit_asm:
-;COMPLETAR
+	blit_asm:
+		push rbp
+		mov rbp, rsp
+		mov eax, edx
+		mul ecx						;[EDX:EAX]
+		mov ecx, edx
+		shl rcx, 32
+		add ecx, eax
+		shr rcx, 2
+		
+		.ciclo:
+			movdqu xmm0, [rdi]
+			movdqu [rsi], xmm0
+			add rsi, 16
+			add rdi, 16
+			loop .ciclo
+
+		pop rbp
 	ret
--- codigo/filtros/edge_asm.asm
+++ codigo/filtros/edge_asm.asm
@@ -1,11 +1,50 @@
+; Parámetros:
+; 	rdi = src
+; 	rsi = dst
+; 	rdx = width
+; 	rcx = height
+; 	r8 = src_row_size
+; 	r9 = dst_row_size
+
 section .data
 DEFAULT REL
 
 section .text
 global edge_asm
 edge_asm:
-;COMPLETAR
-	ret
-		
-		
+	push rbp
+	mov rbp, rsp
 	
+	xor r10, r10
+	mov r10, rcx
+
+	.primeraFila:
+		mov ah, [rdi]
+		mov [rsi], ah
+		lea rdi, [rdi + 1]
+		lea rsi, [rsi + 1]
+		loop .primeraFila
+
+	mov rcx, r10
+	sub rdx, 2
+	mov r11, rdx
+
+	.interno:
+
+		.ciclo:
+			cmp rdx, 0
+			je .proxFila
+			mov ah, [rdi]
+			mov byte [rsi], 0
+			lea rdi, [rdi + 1]
+			lea rsi, [rsi + 1]
+			sub rdx, 1
+			jmp .ciclo
+	
+		.proxFila:
+			mov rdx, r11
+			loop .interno
+
+	.ultimaFila:
+		pop rbp
+		ret
