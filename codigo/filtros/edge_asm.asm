c8845fb0c65642c3d554017fd23001eaa415d0e6
Santiago Tucci <stucci@dc.uba.ar>
2018-05-06 21:49:26 -0300

    .

--- codigo/filtros/edge_asm.asm
+++ codigo/filtros/edge_asm.asm
@@ -7,44 +7,52 @@
 ; 	r9 = dst_row_size
 
 section .data
+	mask1: db 0, 1, 2, 3, 8, 5, 10, 7, 4 ,9 ,6, 11, 12, 13, 14, 15
 DEFAULT REL
 
 section .text
 global edge_asm
-edge_asm:
+edge_asm:									;NOTAR QUE LOS PIXELES MIDEN 1 BYTE
 	push rbp
 	mov rbp, rsp
-	
-	xor r10, r10
-	mov r10, rcx
 
-	.primeraFila:
-		mov ah, [rdi]
-		mov [rsi], ah
-		lea rdi, [rdi + 1]
-		lea rsi, [rsi + 1]
-		loop .primeraFila
+	shr rdx, 4								;voy a procesar de a 16 pixeles/bytes
+	xor r10, r10							;r10 y r11 son contadores para iterar		
+	xor r11, r11
 
-	mov rcx, r10
-	sub rdx, 2
-	mov r11, rdx
+	;COPIO PRIMER FILA SIN MODIFICAR
+	.primerFila:	
+		cmp r11, rdx						;verificar si es la ultima columna de la fila
+		je .edge
+		movdqu xmm0, [rdi]
+		movdqu [rsi], xmm0
+		add rsi, 16
+		add rdi, 16
+		inc r11
+		jmp .primerFila
 
-	.interno:
+	.edge:
+		mov eax, [rdi]
+		pxor xmm2, xmm2
+		pxor xmm1, xmm1
+		pxor xmm0, xmm0
+		movd xmm0, eax
+		punpcklbw xmm0, xmm1
+		movd xmm1, eax
+		punpcklbw xmm1, xmm2
+		pslldq xmm0, 8
+		paddw xmm0, xmm1
 
-		.ciclo:
-			cmp rdx, 0
-			je .proxFila
-			mov ah, [rdi]
-			mov byte [rsi], 0
-			lea rdi, [rdi + 1]
-			lea rsi, [rsi + 1]
-			sub rdx, 1
-			jmp .ciclo
-	
-		.proxFila:
-			mov rdx, r11
-			loop .interno
+		mov eax, [rdi+rdx]
+		pxor xmm3, xmm3
+		pxor xmm2, xmm2
+		pxor xmm1, xmm1
+		movd xmm1, eax
+		punpcklbw xmm1, xmm2
+		movd xmm2, eax
+		punpcklbw xmm2, xmm3
+		pslldq xmm1, 8
+		paddw xmm1, xmm2
 
-	.ultimaFila:
-		pop rbp
-		ret
+	pop rbp
+	ret
