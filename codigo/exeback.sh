bfd8ed4c5645d8934a3f03c06287480bcf116ce0
joa_joa <jromera@dc.uba.ar>
2018-05-16 23:33:03 -0300

    Primer test

--- codigo/exeback.sh
+++ codigo/exeback.sh
@@ -1,13 +1,15 @@
 #!/bin/bash  
 for i in tests/img_generadas/* ;
 do
-    if test -f $i && test ! -x $i ;
-    then
-        for j in {1..100};
-        do
+    echo $i
+    #if test -f $i && test ! -x $i ;
+    #then
+        #for j in {1..100};
+        #do
             #echo $i
-            build/tp2 -i asm -o postfiltro -t 100 ondas $i
-            cat Output.txt >> asmresultadosONDAS.csv
-        done
-    fi
+            build/tp2 -i c -o postfiltro -t 100 temperature $i
+            cat Output.txt >> cresultadosTEMP.csv
+            rm Output.txt
+        #done
+    #fi
 done
--- codigo/tests/1_generar_imagenes_grupo.py
+++ codigo/tests/1_generar_imagenes_grupo.py
@@ -8,23 +8,30 @@ import sys
 # pocas imagenes base.
 
 
-IMAGENES=["lena.bmp","black.bmp","colores32.bmp","white.bmp"]
+IMAGENES=["lena.bmp"]
 
 assure_dirs()
 
 #sizes=['200x200', '204x204', '208x208', '256x256', '512x512', '1024x768']
 
-sizes=['16x16','32x32','64x64','128x128','256x256','512x512','1024x1024']
+sizes=['16x16']
+
+i = 16
+while i < 1024:
+	i += 8
+	sizes += ["{}x{}".format(i,i)]
+
 
 for filename in IMAGENES:
 	print(filename)
-
+	j = 1
 	for size in sizes:
 		sys.stdout.write("  " + size)
 		name = filename.split('.')
 		file_in  = "./img_a_generar/" + filename
-		file_out = "./img_generadas/" + name[0] + "." + size + "." + name[1]
+		file_out = "./img_generadas/" + name[0] + "." + str(j).zfill(3) + "." + size + "." + name[1]
 		resize = "convert -resize " + size + "! " + file_in + " " + file_out
 		subprocess.call(resize, shell=True)
+		j += 1
 
 print("")
